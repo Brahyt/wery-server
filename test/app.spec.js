970a3d4253e0f78d1a3be2f501c4a5cc02fe9010
Daniel Bright <brahyt@gmail.com>
2019-11-21 09:12:06 -0800

    ready for client

--- test/app.spec.js
+++ test/app.spec.js
@@ -38,7 +38,7 @@ describe('App', () => {
     afterEach('Clear data from db', () => {
       fixtures.cleanTables(db);
     });
-    describe.only('/api/characters', () => {
+    describe('/api/characters', () => {
       context('SAD PATH', () => {
         context('INVALID ENDPOINTS', () => {
           it('/GET /characters/:char_id wrong char_id responds with 404', () => {
@@ -74,9 +74,12 @@ describe('App', () => {
             .get('/api/characters/1')
             .then(result => {
               expect(200)
+              expect(result.body.char_id).to.equal(1)
+              expect(result.body.stickers).to.be.an('array')
+              expect(result.body.equipment).to.be.an('object')
             })
         });
-        it('/patch /characters/:char_id responds with 200', () => {
+        it('/PATCH /characters/:char_id responds with 200', () => {
           return supertest(app)
             .patch('/api/characters/1')
             .send(fixtures.testCharacter())
@@ -95,44 +98,47 @@ describe('App', () => {
               expect(response.body.char_id).to.equal(9)
             })
         })
-        it('/delete /characters/:char_id shoule delete a character', () => {
+        it('/DELETE /characters/:char_id should delete a character', () => {
           return supertest(app)
             .delete('/api/characters/1')
             .expect(200)
         })
-        
       })
     });
     describe('/api/parties', () => {
-      it('/GET /api/parties responds with 200', () => {
-        return supertest(app)
-          .get('/api/parties')
-          .then(result => {
-            expect(result.body).to.be.an('array').with.lengthOf(3)
-            expect(result.body[0]).to.eql({party_id:1, name: 'Coolest Test Party'})
-          })
-      });
-      it('/GET /api/parties/:party_id responds with object containing party 1', () => {
-        return supertest(app)
-          .get('/api/parties/1')
-          .then(result => {
-            expect(result.body).to.deep.include({party_id: 1})
-          })
+      context('SAD PATH', () => {
+        it('/GET /api/parties/:party_id responds when choosing a party not there', () => {
+          return supertest(app)
+            .get('/api/parties/111')
+            .expect(404, {error:  "No party with that id"})
+        })
       })
-      it('/GET /api/parties/:party_id responds when choosing a party not there', () => {
-        return supertest(app)
-          .get('/api/parties/111')
-          .expect(404, {error:  "No party with that id"})
+      context('HAPPY PATH', () => {
+        it('/GET /api/parties responds with 200', () => {
+          return supertest(app)
+            .get('/api/parties')
+            .then(result => {
+              expect(result.body).to.be.an('array').with.lengthOf(3)
+              expect(result.body[0]).to.eql({party_id:1, name: 'Coolest Test Party'})
+            })
+        });
+        it('/GET /api/parties/:party_id responds with object containing party 1', () => {
+          return supertest(app)
+            .get('/api/parties/1')
+            .then(result => {
+              expect(result.body).to.deep.include({party_id: 1})
+            })
+        })
+        it('/POST /api/parties creates a party and returns the party created', () => {
+          return supertest(app)
+            .post('/api/parties')
+            .send({name: "New Test Party"})
+            .expect(200, {
+              party_id: 4,
+              name: "New Test Party"
+            });
+        });
       })
-      it('/POST /api/parties creates a party and returns the party created', () => {
-        return supertest(app)
-          .post('/api/parties')
-          .send({name: "New Test Party"})
-          .expect(200, {
-            party_id: 4,
-            name: "New Test Party"
-          });
-      });
     });
     describe('/api/users', () => {
       it('/GET /users responds with 200', () => {
@@ -150,7 +156,6 @@ describe('App', () => {
           });
       });
       it('/POST /user adds user', function() {
-        this.retries(3);
         const newUser = {
           user_email: 'new_user@gmail.com',
           user_password: '123456',
